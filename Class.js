a5f1914ad335ccaffa66da04b480982978135c22
banitlgus <banitlgus@gmail.com>
2025-11-07 15:43:27 +0900

    Creating Forces
    Force 만들기

--- Class.js
+++ Class.js
@@ -1,20 +1,44 @@
 class Mover {
-    constructor() {
-        this.position = createVector();
+    constructor(x, y, mass) {
+        this.mass = mass;
+
+        this.position = createVector(x, y);
         this.velocity = createVector();
         this.acceleration = createVector();
-        this.mass = 10;
+
     }
 
     applyForce(force) {
-        let f = force.copy();
-
-        f.div(this.mass);
+        let f = p5.Vector.div(force, this.mass);
         this.acceleration.add(f);
     }
 
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0);
+    }
+
     show() {
-        circle(this.position.x, this.position.y, 10);
+        stroke(0);
+        fill(175);
+
+        circle(this.position.x, this.position.y, this.mass*16);
+    }
+
+    checkEdges() {
+        if (this.position.x > width) {
+            this.position.x = width;
+            this.velocity.x *= -1;
+        } else if (this.position.x < 0) {
+            this.velocity.x *= -1;
+            this.position.x = 0;
+        }
+
+        if (this.position.y > height) {
+            this.velocity.y *= -1;
+            this.position.y = height;
+        }
     }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,29 @@
+  let moverA, moverB;
+
 function setup() {
-  createCanvas(400, 400);
-  let moverA = new Mover();
-  let moverB = new Mover();
+  createCanvas(800, 400);
+  moverA = new Mover(200, 30, 10);
+  moverB = new Mover(600, 30, 2);
 }
 
 function draw() {
   background(220);
 
-  let wind = createVector(1,0);
+  let gravity = createVector(0, 0.1);
+  moverA.applyForce(gravity);
+  moverB.applyForce(gravity);
 
-  moverA.applyForce(wind);
-  moverB.applyForce(wind);
+  if (mouseIsPressed) {
+    let wind = createVector(0.1, 0);
+    moverA.applyForce(wind);
+    moverB.applyForce(wind);
+  }
+
+  moverA.checkEdges();
+  moverA.update();
+  moverA.show();
+
+  moverB.checkEdges();
+  moverB.update();
+  moverB.show();
 }
